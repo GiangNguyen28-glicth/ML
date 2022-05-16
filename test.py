1e432bbd4789baac8cc6eb6d7257ca8bd0c78f0d
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-05-16 20:52:22 +0700

    update

--- test.py
+++ test.py
@@ -8,10 +8,18 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 data = pd.read_csv('./train-data.csv',index_col="CarId")
 data.shape
 x, y = train_test_split(data, test_size=0.2, random_state=42)
+
 y_train=x["Price"].copy()
+y_train
+
 x_train = x.drop(columns = "Price")
+x_train
+
 y_valid = y["Price"].copy()
+y_valid
+
 x_valid = y.drop(columns = "Price")
+x_valid
 class ColumnSelector(BaseEstimator, TransformerMixin):
     def __init__(self, feature_names):
         self.feature_names = feature_names
@@ -26,7 +34,7 @@ features_cat =["Name","Location","Fuel_Type","Transmission","Owner_Type","Mileag
 cat_pipeline = Pipeline([
     ('selector', ColumnSelector(features_cat)),
     ('imputer', SimpleImputer(missing_values=np.nan, strategy="constant", fill_value = "NO INFO", copy=True)),
-    ('cat_encoder', OneHotEncoder())])
+    ('cat_encoder', OneHotEncoder(handle_unknown='ignore'))])
 
 num_pipeline = Pipeline([
     ('selector', ColumnSelector(features_numbers)),
@@ -39,17 +47,20 @@ full_pipeline = FeatureUnion(transformer_list=[
 
 
 processed_train_set_val = full_pipeline.fit_transform(x_train)
-processed_test_set_val = full_pipeline.fit_transform(x_valid)
+processed_train_set_val.shape
+processed_test_set_val = full_pipeline.transform(x_valid)
+processed_test_set_val.shape
 print('\n____________ Processed feature values ____________')
 print(processed_train_set_val[[0, 1, 2],:].toarray())
 print(processed_train_set_val.shape)
 print('We have %d numeric feature + 1 added features + 35 cols of onehotvector for categorical features.' %(len(features_numbers)))
 
-# from sklearn.tree import DecisionTreeRegressor
-# dt_model=DecisionTreeRegressor(random_state=1)
-# dt_model.fit(processed_train_set_val,y_train)
-# print('\n____________ DecisionTreeRegressor ____________')
+from sklearn.tree import DecisionTreeRegressor
+dt_model=DecisionTreeRegressor(random_state=1)
+dt_model.fit(processed_train_set_val,y_train)
 
+print("\nPredictions: ", dt_model.predict(processed_test_set_val[0:9]))
+print("Labels:      ", list(y_valid[0:9]))
 # def r2score_and_rmse(model, train_data, labels): 
 #     r2score = model.score(train_data, labels)
 #     from sklearn.metrics import mean_squared_error
@@ -61,6 +72,3 @@ print('We have %d numeric feature + 1 added features + 35 cols of onehotvector f
 # print('\nR2 score (on training data, best=1):', r2score)
 # print("Root Mean Square Error: ", rmse.round(decimals=1))
 
-# print("\nInput data: \n", x.iloc[0:9])
-# print("\nPredictions: ", dt_model.predict(processed_train_set_val[0:9]))
-# print("Labels:      ", list(y_train[0:9]))
