08592c863530491c05c9565c11c38302deb35134
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-05-20 14:09:10 +0700

    update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #%%
+import joblib
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
@@ -80,10 +81,6 @@ print('\n____________ Processed feature values ____________')
 print(processed_train_set_val[[0, 1, 2],:].toarray())
 print(processed_train_set_val.shape)
 print('We have %d numeric feature + 1 added features + 35 cols of onehotvector for categorical features.' %(len(features_numbers)))
-#==================================Name====================================
-plt.figure(figsize=(5,5))
-data["Name"].value_counts().plot(kind='bar')
-plt.show()
 #==================================Price===================================
 plt.figure(figsize=(5,5))
 sns.distplot(data["Price"],kde=True)
@@ -160,9 +157,9 @@ print("Labels:      ", list(y_valid[0:9]))
 #===============================Tính Score và RMSE trên GradientBoostingRegressor======================
 #%%
 print("\n____________ Tính Score và RMSE trên GradientBoostingRegressor ____________")
-accuracy_train_p, rmse_train = r2score_and_rmse(grabt_model, processed_train_set_val, y_train)
-print("Độ chính xác trên tập huấn luyện :",accuracy_train_p)
-print("RMSE :",rmse_train)
+accuracy_train_grabt, rmse_train_grabt = r2score_and_rmse(grabt_model, processed_train_set_val, y_train)
+print("Độ chính xác trên tập huấn luyện :",accuracy_train_grabt)
+print("RMSE :",rmse_train_grabt)
 accuracy_test_p, rmse_test = r2score_and_rmse(grabt_model, processed_test_set_val, y_valid)
 print("Độ chính xác trên tập test :",accuracy_test_p)
 print("RMSE :",rmse_test)
@@ -170,10 +167,10 @@ print("RMSE :",rmse_test)
 #%%
 print("\n____________ K-fold cross validion trên GradientBoostingRegressor trên tập Train ____________")
 #Train
-nmse_scores_train = cross_val_score(grabt_model, processed_train_set_val, y_train, cv=cv, scoring='neg_mean_squared_error')
-rmse_scores_train = np.sqrt(-nmse_scores_train)
-print("DecisionTreeRegressor rmse: ", rmse_scores_train.round(decimals=1))
-print("Avg. rmse: ", mean(rmse_scores_train.round(decimals=1)),'\n')
+nmse_scores_train_kfoldgrd = cross_val_score(grabt_model, processed_train_set_val, y_train, cv=cv, scoring='neg_mean_squared_error')
+rmse_scores_train_kfoldgrd = np.sqrt(-nmse_scores_train_kfoldgrd)
+print("GradientBoostingRegressor rmse: ", rmse_scores_train_kfoldgrd.round(decimals=1))
+print("Avg. rmse: ", mean(rmse_scores_train_kfoldgrd.round(decimals=1)),'\n')
 #%%
 #Test
 print("\n____________ K-fold cross validion trên GradientBoostingRegressor trên tập Test ____________")
@@ -205,10 +202,18 @@ random_cv = RandomizedSearchCV(estimator=grabt_model,
                                n_jobs = -1, verbose = 1, 
                                return_train_score = True,
                                random_state=42)
-random_cv.fit(processed_train_set_val, y_train)
-random_results = pd.DataFrame(random_cv.cv_results_).sort_values('mean_test_score', ascending = False)
-random_results.head(10)
-random_cv.best_estimator_
+
+load_randomcv=False
+if load_randomcv: 
+    random_cv.fit(processed_train_set_val, y_train)
+    random_results = pd.DataFrame(random_cv.cv_results_).sort_values('mean_test_score', ascending = False)
+    random_results.head(10)
+    random_cv.best_estimator_
+    joblib.dump(random_cv,'models/GradientBoostingRegressor_RandomizedSearchCV.pkl') 
+else:
+    random_cv = joblib.load('models/GradientBoostingRegressor_RandomizedSearchCV.pkl')
+
+
 #%%
 print("\n____________ GradientBoostingRegressor ____________")
 grabt_model = GradientBoostingRegressor(max_depth = 5,
@@ -231,8 +236,8 @@ plt.figure(figsize=(5,5))
 plt.style.use('fivethirtyeight')
 plt.plot(results['param_n_estimators'], -1 * results['mean_test_score'], label = 'Testing Error')
 plt.plot(results['param_n_estimators'], -1 * results['mean_train_score'], label = 'Training Error')
-plt.xlabel('Number of Trees'); plt.ylabel('Mean Abosolute Error'); plt.legend();
-plt.title('Performance vs Number of Trees');
+plt.xlabel('Number of Trees'); plt.ylabel('Mean Abosolute Error'); plt.legend()
+plt.title('Performance vs Number of Trees')
 results.sort_values('mean_test_score', ascending = False).head(5)
 # %%
 #==============================Final Model=======================
@@ -241,16 +246,141 @@ def mae(y_true, y_pred):
 print("\n____________ Final Model GradientBoostingRegressor ____________")
 default_model_grabt = GradientBoostingRegressor(random_state = 42)
 final_model_grabt = grid_search.best_estimator_
+
 default_model_grabt.fit(processed_train_set_val, y_train)
 final_model_grabt.fit(processed_train_set_val, y_train)
+
 default_pred = default_model_grabt.predict(processed_test_set_val)
 final_pred = final_model_grabt.predict(processed_test_set_val)
+
+
+print(final_pred[0:9])
+print(final_model_grabt.score(processed_test_set_val,y_valid))
+print(default_model_grabt.score(processed_test_set_val,y_valid))
+print(list(y_valid[0:9]))
 print('Default model performance on the test set: MAE = %0.4f.' % mae(y_valid, default_pred))
 print('Final model performance on the test set:   MAE = %0.4f.' % mae(y_valid, final_pred))
+# accuracy_train_grabt_t, rmse_train_grabt_t = r2score_and_rmse(default_model_grabt, processed_test_set_val, y_valid)
+# print("GradientBoostingRegressor Default rmse: ", accuracy_train_grabt_t.round(decimals=1))
+# print("Avg. rmse: ", mean( rmse_train_grabt_t.round(decimals=1)),'\n')
+
+# accuracy_test_grabt_f, rmse_test_grabt_f = r2score_and_rmse(final_model_grabt,processed_test_set_val, y_valid)
+# print("GradientBoostingRegressor Final rmse: ", accuracy_test_grabt_f.round(decimals=1))
+# print("Avg. rmse: ", mean(rmse_test_grabt_f),'\n')
 
 sns.kdeplot(final_pred, label = 'Predictions')
 sns.kdeplot(y_valid, label = 'Values')
 plt.xlabel('Energy Star Score'); 
 plt.ylabel('Density')
 plt.title('Test Values and Predictions')
+
+print('Default model performance on the test set: MAE = %0.4f.' % mae(y_valid, default_pred))
+print('Final model performance on the test set:   MAE = %0.4f.' % mae(y_valid, final_pred))
+
+#==============================RandomForestRegressor=======================
+#%% 5.3 Try RandomForestRegressor model
+# Training (NOTE: may take time if train_set is large)
+from sklearn.ensemble import RandomForestRegressor
+model_random = RandomForestRegressor(n_estimators = 5) # n_estimators: no. of trees
+model_random.fit(processed_train_set_val, y_train)
+# Compute R2 score and root mean squared error
+#%%
+print('\n____________ RandomForestRegressor ____________')
+r2score_model_random, rmse_model_random = r2score_and_rmse(model_random, processed_train_set_val, y_train)
+print('\nR2 score (on training data, best=1):',r2score_model_random)
+print("Root Mean Square Error: ", rmse_model_random.round(decimals=1))     
+# Predict labels for some training instances
+#print("Input data: \n", train_set.iloc[0:9])
+print("\nPredictions: ", model_random.predict(processed_train_set_val[0:9]).round(decimals=1))
+print("Labels:      ", list(y_train[0:9]))
+#%% 5.5 Evaluate with K-fold cross validation 
+print('\n____________ K-fold --> rmse ____________')
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold
+cv = KFold(n_splits=5,shuffle=True,random_state=37) # cv data generator: just a try to persist data splits (hopefully)
+model_name = "RandomForestRegressor" 
+model_random = RandomForestRegressor(n_estimators = 5)
+nmse_scores_train_kfoldrandom = cross_val_score(model_random, processed_train_set_val, y_train, cv=cv, scoring='neg_mean_squared_error')
+rmse_scores_train_kfoldrandom = np.sqrt(-nmse_scores_train_kfoldrandom)
+print("RandomForestRegressor rmse: ", rmse_scores_train_kfoldrandom.round(decimals=1))
+print("Avg. rmse: ", mean(rmse_scores_train_kfoldrandom.round(decimals=1)),'\n')
+
+# %% Fine-turn tìm ra hyperparameter định hình nên model ( phải đưa dô trc khi training )
+from sklearn.model_selection import GridSearchCV
+cv = KFold(n_splits=5,shuffle=True,random_state=37) # cv data generator
+model_random = RandomForestRegressor()
+param_grid = [
+    # try 15 (3x4) combinations of hyperparameters (bootstrap=True: drawing samples with replacement)
+    {'bootstrap': [True], 'n_estimators': [3, 15, 30], 'max_features': [2, 12, 20, 39]},
+    # then try 12 (4x3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 5, 10, 20], 'max_features': [2, 6, 10]} ]
+    # Train across 5 folds, hence a total of (15+12)*5=135 rounds of training 
+grid_search = GridSearchCV(model_random, param_grid, cv=cv, scoring='neg_mean_squared_error', return_train_score=True, 
+refit=True) # refit=True: after finding best hyperparam, it fit() the model with whole data (hope to get better result)
+grid_search.fit(processed_train_set_val, y_train)
+print_search_result(grid_search, model_name = "RandomForestRegressor")   
+
+#%%
+print("\n____________ K-fold cross validion trên GradientBoostingRegressor trên tập Train ____________")
+#Train
+nmse_scores_train_kfoldgrd = cross_val_score(grabt_model, processed_train_set_val, y_train, cv=cv, scoring='neg_mean_squared_error')
+rmse_scores_train_kfoldgrd = np.sqrt(-nmse_scores_train_kfoldgrd)
+print("GradientBoostingRegressor rmse: ", rmse_scores_train_kfoldgrd.round(decimals=1))
+print("Avg. rmse: ", mean(rmse_scores_train_kfoldgrd.round(decimals=1)),'\n')
+# %%
+#==============================Feature Importances=======================
+best_model = grid_search.best_estimator_
+feature_importances = best_model.feature_importances_
+feature_names = x_train.columns.tolist()
+for name in features_cat:
+    feature_names.remove(name)
+print('\nFeatures and importance score: ')
+values_importance = []
+feature_names_new=[]
+for importance,name in sorted(zip(feature_importances,feature_names),reverse=True):
+    values_importance.append(importance)
+    feature_names_new.append(name)
+    print((importance,name))
+feature_results = pd.DataFrame({'feature': feature_names_new, 
+                                'importance':  values_importance})
+plt.figure(figsize=(5,5))
+feature_results.loc[:9, :].plot(x = 'feature', y = 'importance', 
+                                 edgecolor = 'k',
+                                 kind='barh', color = 'blue')
+plt.xlabel('Relative Importance', size = 20)
+plt.ylabel('')
+plt.title('Feature Importances from Random Forest', size = 30)
+    # Print features and importance score  (ONLY on rand forest)
+    
+# %%
+model_comparison = pd.DataFrame({'model':['Gradient Boosted','RandomForestRegressor'],
+                                'RMSE':[rmse_train_grabt,rmse_model_random]})
+model_comparison.sort_values('RMSE',ascending = False).plot(x = 'model',
+                                                           y = 'RMSE',
+                                                           kind = 'barh',
+                                                           color = 'red', 
+                                                           edgecolor = 'black')
+plt.ylabel('')
+plt.yticks(size = 14)
+plt.xlabel('Mean Absolute Error')
+plt.xticks(size = 14)
+plt.title('RMSE', size = 20)
+plt.show()
+
+#%%
+avg_rmse_kfoldgrd=mean(rmse_scores_train_kfoldgrd.round(decimals=1))
+avg_rmse_kfoldrandom=mean(rmse_scores_train_kfoldrandom.round(decimals=1))
+model_comparison = pd.DataFrame({'model':['Gradient Boosted','RandomForestRegressor'],
+                                'RMSE':[avg_rmse_kfoldgrd,avg_rmse_kfoldrandom]})
+model_comparison.sort_values('RMSE',ascending = False).plot(x = 'model',
+                                                           y = 'RMSE',
+                                                           kind = 'barh',
+                                                           color = 'red', 
+                                                           edgecolor = 'black')
+plt.ylabel('')
+plt.yticks(size = 14)
+plt.xlabel('Mean Absolute Error')
+plt.xticks(size = 14)
+plt.title('RMSE', size = 20)
+plt.show()
 # %%
