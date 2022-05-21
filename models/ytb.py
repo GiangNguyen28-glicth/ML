ac26e97a4620e198da740b62fc4b8ba90cd6c79b
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-05-21 19:18:46 +0700

    update

--- models/ytb.py
+++
@@ -1,8 +0,0 @@
-import pandas as pb
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
-from sklearn import metrics
-data = pb.read_csv("./train-data")
